feabc9ef34154c25ec61c7311904d06aea7d6f58
awsompankaj <awsompankaj@gmail.com>
2022-01-07 16:09:31 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,12 +4,12 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.small"
-  key_name = "gds-import"
+  key_name = "pankaj-key"
 
   tags = {
-    Name = "Gagan-devops-server-v2"
+    Name = "pankaj-devops-server-v2"
     env = "production"
-    owner = "Vinay"
+    owner = "pankaj"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
